6ffd0b79715676e649d0f046b17ff75b3f4d2964
kampusvn <kampuslearntocode@gmail.com>
2018-05-03 16:03:48 +0700

    new

---
+++ 2.14-MobileCare-Dap-an/CatalogActivity.java
@@ -0,0 +1,35 @@
+﻿public class CatalogActivity extends AppCompatActivity {
+
+    private PetDbHelper mDbHelper;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        
+        ...
+
+        mDbHelper = new PetDbHelper(this);
+        displayDatabaseInfo();
+    }
+    
+    ...
+    
+   /**
+     * Phương thức trợ giúp để chèn dữ liệu thiết bị được mã hóa vào cơ sở dữ liệu. Chỉ dành cho mục đích debug.
+     */
+    private void insertPet() {
+        // TODO: Chèn một thiết bị vào cơ sở dữ liệu
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+		// Người dùng bấm vào một lựa chọn trên menu trong app bar
+        switch (item.getItemId()) {
+			// Phản hồi cho thao tác bấm vào lựa chọn "Thêm dữ liệu giả"
+            case R.id.action_insert_dummy_data:
+                insertPet();
+                displayDatabaseInfo();
+                return true;
+         
+         ...
+    }
+}
