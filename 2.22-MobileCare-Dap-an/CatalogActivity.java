4eb58e09e7dc3415cccf90564789dc761dfc180d
kampusvn <kampuslearntocode@gmail.com>
2018-05-04 09:24:14 +0700

    new

---
+++ 2.22-MobileCare-Dap-an/CatalogActivity.java
@@ -0,0 +1,34 @@
+﻿TextView displayView = (TextView) findViewById(R.id.text_view_mobile);
+
+try {
+    // Tạo header trong Text View như sau:
+    //
+    // Bảng thiết bị chứa <số hàng trong con trỏ> thiết bị
+    // _id - name - breed - gender - weight
+    //
+	// Trong vòng lặp dưới đây, lặp qua từng hàng của con trỏ và hiển thị thông tin
+	// từ mỗi cột theo thứ tự này.
+    displayView.setText("Bảng chứa " + cursor.getCount() + " thiết bị.\n\n");
+    displayView.append(MobileEntry._ID + " - " +
+            MobileEntry.COLUMN_MOBILE_NAME + "\n");
+
+    // Lấy chỉ số của mỗi cột
+    int idColumnIndex = cursor.getColumnIndex(MobileEntry._ID);
+    int nameColumnIndex = cursor.getColumnIndex(MobileEntry.COLUMN_MOBILE_NAME);
+
+    // Lặp qua các hàng được trả về trong con trỏ
+    while (cursor.moveToNext()) {
+		// Sử dụng chỉ số đó để trích xuất giá trị String hoặc Int của thiết bị
+		// tại hàng hiện tại mà con trỏ đang trỏ tới.
+        int currentID = cursor.getInt(idColumnIndex);
+        String currentName = cursor.getString(nameColumnIndex);
+		// Hiển thị giá trị từ mỗi cột của dòng hiện tại trong con trỏ ở TextView
+        displayView.append(("\n" + currentID + " - " +
+                currentName));
+    }
+} finally {
+    // Always close the cursor when you're done reading from it. This releases all its
+    // resources and makes it invalid.
+	// Luôn đóng con trỏ sau khi đọc để giải phóng tài nguyên.
+    cursor.close();
+}
